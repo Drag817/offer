9557ca64633ffaa503922d6da347bac9f6d3c71d
Drag817 <66403146+Drag817@users.noreply.github.com>
2020-12-09 21:25:12 +0900

    Echo bot release

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('Hello World!')
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+bot = Bot(token='1160713030:AAF2luS9Rof5f9YRnihjKOAIN4PGfv3wuH4')
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
