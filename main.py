79b5f45dc29176a64bf4a1a5d8ed7d22a7c890a5
Drag817 <66403146+Drag817@users.noreply.github.com>
2020-12-14 11:51:32 +0900

    add imgs

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import logging
 import requests
 from bs4 import BeautifulSoup
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.utils.markdown import bold
+from aiogram.types import ParseMode
 
 from api_token import token
 
@@ -14,12 +16,14 @@ dp = Dispatcher(bot)
 
 
 def parse(article):
-    url = f"https://novosibirskshop.e2e4online.ru/shop/catalog/xmlPrice.faces?search={article}"
+    url = f"https://novosibirskshop.e2e4online.ru/shop/catalog/item/" \
+          f"?id={article}"
     print(url)
 
     headers = {
         'accept': '*/*',
-        'user - agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0',
+        'user - agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) '
+                        'Gecko/20100101 Firefox/83.0',
     }
 
     req = requests.get(url, headers=headers)
@@ -32,10 +36,35 @@ def parse(article):
         src = file.read()
 
     soup = BeautifulSoup(src, 'lxml')
-    product_title = soup.find("name").get('value')
+
+    product_title = (soup.find("div", class_='itemBlock').
+                     find("h1").text.split('\n'))[0]
     print(product_title)
 
-    return product_title
+    product_price = soup.find("span", class_='price').text
+    product_price = bold(f"Закуп: {product_price}р")
+    # print(product_price)
+
+    answer = f"{product_title}\n\n{product_price}"
+
+    imgs = soup.find_all("a", class_='fancybox-thumb')
+    product_imgs = []
+
+    for img in imgs[:2]:
+        product_imgs.append(img.get('href'))
+
+    print(product_imgs)
+    return answer, product_imgs
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Привет!\nЯ подготовлю КП!")
+
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply("Отправь мне арткул товара")
 
 
 @dp.message_handler()
@@ -45,8 +74,13 @@ async def echo(message: types.Message):
             article = message.text
             print(article)
             print(type(article))
-            answer = parse(article)
-            await message.answer(str(answer))
+            answer, imgs = parse(article)
+
+            await message.answer(str(answer), parse_mode=ParseMode.MARKDOWN)
+
+            for img in imgs:
+                await bot.send_photo(message.chat.id,
+                                     types.InputFile.from_url(img))
     else:
         await message.answer('Нет, Валера, не так...')
 
